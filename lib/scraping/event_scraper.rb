74f2d05b51fd652c632839dc269ce1ece9137861
Sharina Alves <sharina.alves@mail.de>
2019-12-18 16:51:19 +0100

    add scrapeing with mechanize which clicks on search results to get data

--- lib/scraping/event_scraper.rb
+++ lib/scraping/event_scraper.rb
@@ -1,18 +1,30 @@
 require 'nokogiri'
 require 'open-uri'
-# require 'mechanize'
+require 'mechanize'
 
 class EventScraper
+
+  def initialize
+    @agent = Mechanize.new
+  end
+
   def scrape_event_names
     url = "https://de.partybeep.com/Startseite/Oldenburg"
 
     html_file = open(url).read
     html_doc = Nokogiri::HTML(html_file)
+    page = @agent.get(url)
 
     html_doc.search('.ll-ename a').each do |element|
       if Event.where(name: element.text.strip) == []
-        byebug
-        Event.create(name: element.text.strip, category_id: 2)
+        # Event.create(name: element.text.strip, category_id: 2)
+        #byebug
+        link = page.link_with(text: element.text.strip)
+        clicked_page = link.click
+        Event.create(
+          category_id: 2,
+          time: clicked_page.at('.ev-time').text.strip,
+          name: clicked_page.search('#ev-name').text.strip)
       end
     end
   end
