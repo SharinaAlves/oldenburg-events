bdc944a2d411f74d29e9f656cd2118772e334648
Sharina Alves <sharina.alves@mail.de>
2019-12-18 11:42:12 +0100

    add devise

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,11 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @events = policy_scope(Event)
   end
 
   def show
-    #authorize @event
+    authorize @event
   end
 end
