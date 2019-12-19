e39c87fbac1ec8f9a1f36a1591e36051ebb82870
Sharina Alves <sharina.alves@mail.de>
2019-12-19 13:41:18 +0100

    add category controller, routes and basic view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   devise_for :users
-  get 'events/index'
-  get 'events/show'
-  #devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/home'
   #root to: 'pages#home'
-  root to: 'events#index'
+  root to: 'categories#index'
   resources :events, controller: :events, only: [:index, :show]
+  resources :categories, controller: :categories, only: :index
 end
