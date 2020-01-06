280f6996bcfba1ed215b4de68e176dbddb7dedc6
Sharina Alves <sharina.alves@mail.de>
2020-01-06 22:47:43 +0100

    change image format

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,13 @@ Category.delete_all
 
 puts "create"
 
-Category.create(name: "Alle Veranstaltungen", image: "category-img/allevents.jpeg")
-Category.create(name: "Nachtleben", image: "category-img/party.jpeg")
-Category.create(name: "Filme", image: "category-img/cinema.jpeg")
-Category.create(name: "Märkte", image: "category-img/market.jpeg")
-Category.create(name: "Vorträge", image: "category-img/speech.jpeg")
-Category.create(name: "Ausstellungen", image: "category-img/exhibition.jpeg")
-Category.create(name: "Bühne", image: "category-img/theater.jpeg")
-Category.create(name: "Konzerte", image: "category-img/concert.jpeg")
+Category.create(name: "Alle Veranstaltungen", image: "category-img/allevents.jpg")
+Category.create(name: "Nachtleben", image: "category-img/party.jpg")
+Category.create(name: "Filme", image: "category-img/cinema.jpg")
+Category.create(name: "Märkte", image: "category-img/market.jpg")
+Category.create(name: "Vorträge", image: "category-img/speech.jpg")
+Category.create(name: "Ausstellungen", image: "category-img/exhibition.jpg")
+Category.create(name: "Bühne", image: "category-img/theater.jpg")
+Category.create(name: "Konzerte", image: "category-img/concert.jpg")
 
 puts "done"
