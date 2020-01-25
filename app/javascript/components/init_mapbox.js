065b85b3154769897b15149da771468e7fc25b93
Sharina Alves <sharina.alves@mail.de>
2020-01-25 23:14:45 +0100

    add cards to map

--- app/javascript/components/init_mapbox.js
+++ app/javascript/components/init_mapbox.js
@@ -14,8 +14,18 @@ const buildMap = () => {
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+    //const element = document.createElement('div');
+    //element.className = 'marker';
+    //element.style.backgroundImage = `url('${marker.image_url}')`;
+    //element.style.backgroundSize = 'cover';
+    //element.style.width = '38px';
+    //element.style.height = '45px';
+
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
       .addTo(map);
   });
 };
