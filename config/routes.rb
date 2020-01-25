9bf17e3d40fdcdc379e3a2368aee930c2caa9d2e
Sharina Alves <sharina.alves@mail.de>
2020-01-25 21:35:40 +0100

    g user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   require "sidekiq/web"
   authenticate :user, lambda { |u| u.admin } do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_124635) do
+ActiveRecord::Schema.define(version: 2020_01_25_201310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_12_20_124635) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.string "ticket"
     t.index ["category_id"], name: "index_events_on_category_id"
   end
 
