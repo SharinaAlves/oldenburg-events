ed65dd477dd479be5ad656d3a3947ddd7d732f79
Sharina Alves <sharina.alves@mail.de>
2020-01-31 18:20:55 +0100

    add clubnames to scrape methods

--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -5,17 +5,29 @@ class ScrapeJob < ApplicationJob
 
   def perform
     puts "Nachtleben"
-    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/LoftOldenburg/events/?ref=page_internal") # Loft
-    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/amadeus.oldenburg/events/?ref=page_internal") # Amadeus
-    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/Gesellig.Oldenburg/events/?ref=page_internal") # Gesellig
-    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/CubesOldenburg/events/?ref=page_internal") # Cubes
+    puts "Loft"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/LoftOldenburg/events/?ref=page_internal")
+    puts "Amadeus"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/amadeus.oldenburg/events/?ref=page_internal")
+    puts "Gesellig"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/Gesellig.Oldenburg/events/?ref=page_internal")
+    puts "Cubes"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/CubesOldenburg/events/?ref=page_internal")
     rescue => error
-    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/studiob.oldenburg/events/?ref=page_internal") # Studio B
-    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/PolyesterKlub/events/?ref=page_internal") # Polyester
-    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/metrooldenburg/events/?ref=page_internal") # Metro
-    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/betweenthesheets.oldenburg/events/?ref=page_internal") # Between the Sheets
-    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/molkereiklub/events/?ref=page_internal") # Molkerei Klub
-    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/umBAUbar/events/?ref=page_internal") # Umbaubar
+    puts "Studio B"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/studiob.oldenburg/events/?ref=page_internal")
+    puts "Polyester"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/PolyesterKlub/events/?ref=page_internal")
+    puts "Metro"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/metrooldenburg/events/?ref=page_internal")
+    puts "Between the Sheets"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/betweenthesheets.oldenburg/events/?ref=page_internal")
+    puts "Molkerei Klub"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/molkereiklub/events/?ref=page_internal")
+    puts "umBAUbar"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/umBAUbar/events/?ref=page_internal")
+    puts "Cadillac"
+    EventScraper.new.scrape_facebook("https://www.facebook.com/pg/cadillac.zentrumfurjugendkultur/events/?ref=page_internal")
 
     puts "Delete old ones"
     Event.where("date < ?", Date.today).delete_all
