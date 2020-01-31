92f540959c6e82e847dfbbb7d3149cd01cedaad0
Sharina Alves <sharina.alves@mail.de>
2020-01-31 18:27:30 +0100

    add rescue to every method call

--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -7,27 +7,37 @@ class ScrapeJob < ApplicationJob
     puts "Nachtleben"
     puts "Loft"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/LoftOldenburg/events/?ref=page_internal")
+    rescue => error
     puts "Amadeus"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/amadeus.oldenburg/events/?ref=page_internal")
+    rescue => error
     puts "Gesellig"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/Gesellig.Oldenburg/events/?ref=page_internal")
+    rescue => error
     puts "Cubes"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/CubesOldenburg/events/?ref=page_internal")
     rescue => error
     puts "Studio B"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/studiob.oldenburg/events/?ref=page_internal")
+    rescue => error
     puts "Polyester"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/PolyesterKlub/events/?ref=page_internal")
+    rescue => error
     puts "Metro"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/metrooldenburg/events/?ref=page_internal")
+    rescue => error
     puts "Between the Sheets"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/betweenthesheets.oldenburg/events/?ref=page_internal")
+    rescue => error
     puts "Molkerei Klub"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/molkereiklub/events/?ref=page_internal")
+    rescue => error
     puts "umBAUbar"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/umBAUbar/events/?ref=page_internal")
+    rescue => error
     puts "Cadillac"
     EventScraper.new.scrape_facebook("https://www.facebook.com/pg/cadillac.zentrumfurjugendkultur/events/?ref=page_internal")
+    rescue => error
 
     puts "Delete old ones"
     Event.where("date < ?", Date.today).delete_all
