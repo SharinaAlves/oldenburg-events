3c7c9328308f446195751497709dfc720fa23660
Sharina Alves <sharina.alves@mail.de>
2020-03-03 22:30:00 +0100

    add very basic datepicker without implementing it to search yet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import '../components/flatpickr';
 
 import { getUserLocation } from '../components/get_user_location';
 import { initMapbox } from '../components/init_mapbox';
