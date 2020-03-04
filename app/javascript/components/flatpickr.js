95da2dae16766263e8a94acef2ae2ffd90a95b30
Sharina Alves <sharina.alves@mail.de>
2020-03-04 21:35:04 +0100

    add range to date and add date to search

--- app/javascript/components/flatpickr.js
+++ app/javascript/components/flatpickr.js
@@ -1,4 +1,13 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
 
-flatpickr(".datepicker", {})
+flatpickr(".datepicker", {
+  enableTime: true,
+  dateFormat: "d.m.Y",
+  minDate: "today",
+  mode: "range",
+  onChange: function(selectedDates) {
+    let from = selectedDates[0];
+    let to = selectedDates[1];
+  };
+});
